ba10bcf251d83ad3313fb86bd9c3e9189dc25e37
Vishnu Chandran <vishnuchandran5555@gmail.com>
2024-07-30 14:36:11 +0530

    clean

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeComp1 from '../components/HomeComp1.vue'
 </script>
 
 <template>
   <div class="row">
     <h1>Home page page</h1>
-    <TheWelcome />
+    <HomeComp1 />
   </div>
 </template>
